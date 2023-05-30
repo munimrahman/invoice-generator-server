ec3409b04a2cc08649f8c7a727fe359815f6b782
munimrahman <munimrh@gmail.com>
2023-05-30 16:23:52 +0600

    compare pass

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,10 @@
+/* eslint-disable consistent-return */
+/* eslint-disable func-names */
+/* eslint-disable prettier/prettier */
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const validator = require('validator');
+const crypto = require('crypto');
 
 const { Schema } = mongoose;
 
@@ -14,7 +18,7 @@ const userSchema = new Schema(
     },
     email: {
       type: String,
-      validator: [validator.isEmail, 'Provide a Valid Email Address'],
+      validate: [validator.isEmail, 'Provide a Valid Email Address'],
       required: true,
       unique: true,
       trim: true,
@@ -22,7 +26,7 @@ const userSchema = new Schema(
     },
     mobile: {
       type: String,
-      validator: [validator.isMobilePhone, 'bn-BD', 'Provide a Valid BD Mobile Number'],
+      validate: [validator.isMobilePhone, 'bn-BD', 'Provide a Valid BD Mobile Number'],
       unique: true,
       trim: true,
       lowercase: true,
@@ -35,15 +39,36 @@ const userSchema = new Schema(
     password: {
       type: String,
       required: [true, 'Password is required'],
+      validate: {
+        validator: (value) => validator.isStrongPassword(value, {
+            minLength: 6,
+            minLowerCase: 1,
+            minUpperCase: 1,
+            minNumbers: 1,
+            minSymbols: 1,
+          }),
+          message: 'Password {VALUE} is not strong enough.'
+      },
     },
     confirmPassword: {
       type: String,
       required: [true, 'Please confirm your password'],
+      validate: {
+        validator(value) {
+          return value === this.password;
+        },
+        message: 'Passwords does not match!'
+      }
     },
     role: {
       type: String,
       enum: ['user', 'admin'],
       default: 'user',
+    },
+    status: {
+      type: String,
+      default: 'inactive',
+      enum: ['active', 'inactive', 'blocked'],
     },
 
     confirmationToken: String,
@@ -58,4 +83,37 @@ const userSchema = new Schema(
   }
 );
 
-module.exports = mongoose.model('User', userSchema);
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const { password } = this;
+  const hashedPassword = bcrypt.hashSync(password);
+
+  this.password = hashedPassword;
+  this.confirmPassword = undefined;
+
+  next();
+});
+
+userSchema.methods.comparePassword = function (password, hash) {
+  const isPasswordValid = bcrypt.compareSync(password, hash);
+  return isPasswordValid;
+};
+
+userSchema.methods.generateConfirmationToken = function () {
+  const token = crypto.randomBytes(32).toString('hex');
+
+  this.confirmationToken = token;
+
+  const date = new Date();
+
+  date.setDate(date.getDate() + 1);
+  this.confirmationTokenExpires = date;
+
+  return token;
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
